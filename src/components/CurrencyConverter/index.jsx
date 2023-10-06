3a0f027479c18732207f7a17411f7adbe7ebb1b1
Jonathan de Oliveira <jonathandeoliveirarocha2002@gmail.com>
2023-10-06 19:08:53 -0300

    CurrencyConverter Axios api

--- src/components/CurrencyConverter/index.jsx
+++ src/components/CurrencyConverter/index.jsx
@@ -1,14 +1,44 @@
-import React from "react"
-import * as D from "./styles"
+import React from "react";
+import { useState, useEffect } from "react";
+import * as D from "./styles";
+import Axios from "axios";
 
-const CurrencyConverter = () =>{
-    return (
-        <> 
-           <D.Main>
-                <h1>Conversor de Moedas</h1>
-           </D.Main>
-        </>
-                  
-    )
-}
-export default CurrencyConverter
+const CurrencyConverter = () => {
+  const [from, setFrom] = useState("brl");
+  const [price, setPrice] = useState([]);
+  const [input, setInput] = useState(0);
+
+  useEffect(() => {
+    Axios.get(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}.json`
+    ).then((res) => {
+      setPrice(res.data[from]);
+    });
+  }, [from]);
+
+  useEffect(() => {
+    convert();
+  }, [price]);
+
+  function convert() {
+    setInput(document.querySelector("#price").value);
+  }
+
+  return (
+    <>
+      <D.Main>
+        <h1>Conversor de Moedas</h1>
+        <input
+          onChange={convert}
+          min="0"
+          type="number"
+          id="price"
+          placeholder="Valor (R$)"
+        />
+        <p>USD: ${(input * price["usd"]).toFixed(2)}</p>
+        <p>EUR: ${(input * price["eur"]).toFixed(2)}</p>
+      </D.Main>
+    </>
+  );
+};
+export default CurrencyConverter;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,55 @@
-import React from "react"
-import * as H from "./styles"
+import React from "react";
+import * as H from "./styles";
 
-const Header = (props) =>{
-    return (
-        <>
-            <H.Header>
-                <H.Link href="#Home" onClick={() => {props.setPage('Home')}}><H.Logo>Financeir.io</H.Logo></H.Link>
-                
-                <H.Nav>
-                    <H.Link href="#MultaPorAtraso" onClick={() => {props.setPage('LateFee')}}>Multa por Atraso</H.Link>
-                    <H.Link href="#ConversorDeMoedas" onClick={() => {props.setPage('CurrencyConverter')}}>Conversor de Moedas</H.Link>
-                    <H.Link href="#JurosCompostos" onClick={() => {props.setPage('CompoundInterest')}}>Juros Compostos</H.Link>
-                    <H.Link href="#ControleFinanceiro" onClick={() => {props.setPage('FinancialControl')}}>Controle Financeiro</H.Link>
-                </H.Nav>
-            </H.Header>  
-        </>
-                  
-    )
-}
-export default Header
+const Header = (props) => {
+  return (
+    <>
+      <H.Header>
+        <H.Link
+          href="#Home"
+          onClick={() => {
+            props.setPage("Home");
+          }}
+        >
+          <H.Logo>Financeir.io</H.Logo>
+        </H.Link>
+
+        <H.Nav>
+          <H.Link
+            href="#MultaPorAtraso"
+            onClick={() => {
+              props.setPage("LateFee");
+            }}
+          >
+            Multa por Atraso
+          </H.Link>
+          <H.Link
+            href="#ConversorDeMoedas"
+            onClick={() => {
+              props.setPage("CurrencyConverter");
+            }}
+          >
+            Conversor de Moedas
+          </H.Link>
+          <H.Link
+            href="#JurosCompostos"
+            onClick={() => {
+              props.setPage("CompoundInterest");
+            }}
+          >
+            Juros Compostos
+          </H.Link>
+          <H.Link
+            href="#ControleFinanceiro"
+            onClick={() => {
+              props.setPage("FinancialControl");
+            }}
+          >
+            Controle Financeiro
+          </H.Link>
+        </H.Nav>
+      </H.Header>
+    </>
+  );
+};
+export default Header;
