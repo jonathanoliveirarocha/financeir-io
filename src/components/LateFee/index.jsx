99e3709f81b34393fc0791ff8a13b4791d6d3bf4
Jonathan de Oliveira <jonathandeoliveirarocha2002@gmail.com>
2023-10-07 22:04:50 -0300

    LateFee document formated

--- src/components/LateFee/index.jsx
+++ src/components/LateFee/index.jsx
@@ -1,36 +1,43 @@
-import React from "react"
-import { useState } from "react"
-import * as D from "./styles"
+import React from "react";
+import { useState } from "react";
+import * as D from "./styles";
 
 const LateFee = () => {
-  const [value, setValue] = useState(0)
-  const [fees, setFees] = useState(0)
-  const [typeFees, setTypeFees] = useState("daily")
-  const [penalty, setPenalty] = useState(0)
-  const [days, setDays] = useState(0)
-  const [result, setResult] = useState([0,0,0])
+  const [value, setValue] = useState(0);
+  const [fees, setFees] = useState(0);
+  const [typeFees, setTypeFees] = useState("daily");
+  const [penalty, setPenalty] = useState(0);
+  const [days, setDays] = useState(0);
+  const [result, setResult] = useState([0, 0, 0]);
 
-  function calculate(){
-    let tempTypeFees = typeFees==="yearly"?(1 + fees/100) ** (1/365) - 1:typeFees==="monthly"?(1 + fees/100) ** (1/30) - 1:fees/100
-    let tempPenality = value * (penalty/100)
-    let amount = (value * (1+penalty/100)) * (1 + tempTypeFees) ** days
-    let tempFees = amount-(value+tempPenality)
-    setResult([tempPenality,tempFees,amount])
+  function calculate() {
+    let tempTypeFees =
+      typeFees === "yearly"
+        ? (1 + fees / 100) ** (1 / 365) - 1
+        : typeFees === "monthly"
+        ? (1 + fees / 100) ** (1 / 30) - 1
+        : fees / 100;
+    let tempPenality = value * (penalty / 100);
+    let amount = value * (1 + penalty / 100) * (1 + tempTypeFees) ** days;
+    let tempFees = amount - (value + tempPenality);
+    setResult([tempPenality, tempFees, amount]);
   }
   return (
     <>
       <D.Main>
         <h1>Multa por Atraso</h1>
         <label>Valor do Débito: </label>
-        <input type="number" 
-            value={value}
-            onChange={(e) => setValue(parseFloat(e.target.value))}
+        <input
+          type="number"
+          value={value}
+          onChange={(e) => setValue(parseFloat(e.target.value))}
         />
         <br />
         <label>Taxa de Juros (%): </label>
-        <input type="number"
-            value={fees}
-            onChange={(e) => setFees(parseFloat(e.target.value))}
+        <input
+          type="number"
+          value={fees}
+          onChange={(e) => setFees(parseFloat(e.target.value))}
         />
         <select onChange={(e) => setTypeFees(e.target.value)}>
           <option value="daily">Diário</option>
@@ -39,15 +46,17 @@ const LateFee = () => {
         </select>
         <br />
         <label>Multa (%): </label>
-        <input type="number"
-            value={penalty}
-            onChange={(e) => setPenalty(parseFloat(e.target.value))}
+        <input
+          type="number"
+          value={penalty}
+          onChange={(e) => setPenalty(parseFloat(e.target.value))}
         />
         <br />
         <label>Dias em Atraso: </label>
-        <input type="number"
-            value={days}
-            onChange={(e) => setDays(parseFloat(e.target.value))}
+        <input
+          type="number"
+          value={days}
+          onChange={(e) => setDays(parseFloat(e.target.value))}
         />
         <br />
         <button onClick={calculate}>Calcular</button>
